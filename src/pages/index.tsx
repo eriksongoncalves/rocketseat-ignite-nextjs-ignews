ca20a39b96a8a248933b56d04856b469166caebf
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-25 15:47:40 -0300

    feat: add sass, global styles and fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
 }
