7952583933788bccf1554b84df068d0363b43269
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-25 18:35:03 -0300

    feat: signin and signout

---
+++ src/@types/node/enviroment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      STRIPE_API_KEY: string;
+      GITHUB_CLIENT_ID: string;
+      GITHUB_CLIENT_SECRET: string;
+      DATABASE_URL: string;
+    }
+  }
+}
+
+export {};
