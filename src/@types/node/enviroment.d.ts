dd23578f1453267da69eaa92543052d0ae4d3969
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-29 10:43:44 -0300

    feat: config checkout

--- src/@types/node/enviroment.d.ts
+++ src/@types/node/enviroment.d.ts
@@ -2,9 +2,12 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       STRIPE_API_KEY: string;
+      NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
       GITHUB_CLIENT_ID: string;
       GITHUB_CLIENT_SECRET: string;
       FAUNADB_KEY: string;
+      STRIPE_SUCCESS_URL: string;
+      STRIPE_CANCEL_URL: string;
     }
   }
 }
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
+import { signIn, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -6,8 +10,33 @@ interface SubscribeButtonProps {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe!.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
