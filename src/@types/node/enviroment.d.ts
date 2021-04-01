866d91156b34390efe01629e8b9570492d57a36a
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-01 19:14:50 -0300

    feat: add prismic

--- src/@types/node/enviroment.d.ts
+++ src/@types/node/enviroment.d.ts
@@ -9,6 +9,8 @@ declare global {
       GITHUB_CLIENT_ID: string;
       GITHUB_CLIENT_SECRET: string;
       FAUNADB_KEY: string;
+      PRISMIC_ENDPOINT: string;
+      PRISMIC_ACCESS_TOKEN: string;
     }
   }
 }
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,80 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updateAt: string;
+};
+
+type PostsProps = {
+  posts: Post[];
+};
+
+type Content = {
+  type: string;
+};
+
+function Posts({ posts }: PostsProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <a href="#" key={post.slug}>
+              <time>{post.updateAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find(
+          (content: Content) => content.type === 'paragraph'
+        )?.text ?? '',
+      updateAt: new Date(post.last_publication_date!).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }
+      )
+    };
+  });
+
+  return {
+    props: { posts }
+  };
+};
+
+export default Posts;
